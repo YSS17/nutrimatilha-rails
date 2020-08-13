b73bfeb211a47661ae32177a60719c059ac89d84
Isabela Porto <isamporto@hotmail.com>
2020-08-13 01:08:59 -0300

    Added pet info routes, add params to devise and simple form and fixed some containers

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_171155) do
+ActiveRecord::Schema.define(version: 2020_08_13_033633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pet_infos", force: :cascade do |t|
+    t.string "name"
+    t.string "race"
+    t.string "sex"
+    t.date "date_birth"
+    t.float "weight"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pet_infos_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "flavor"
+    t.integer "quantity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +44,12 @@ ActiveRecord::Schema.define(version: 2020_07_02_171155) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pet_infos", "users"
+  add_foreign_key "products", "users"
 end
