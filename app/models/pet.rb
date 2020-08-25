b299750349210d9f8ddeaf8f7af9607045b1f9c0
isabela porto <isamporto@hotmail.com>
2020-08-25 16:49:31 -0300

    Fixed conflicts and added relations between db

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,5 @@
 class Pet < ApplicationRecord
   belongs_to :breed
   belongs_to :user
+  has_one :subs_product
 end
--- db/migrate/20200702171155_devise_create_users.rb
+++ db/migrate/20200702171155_devise_create_users.rb
@@ -27,15 +27,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.inet     :current_sign_in_ip
       # t.inet     :last_sign_in_ip
 
-<<<<<<< HEAD
       # Confirmable
       t.string   :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
 
-=======
->>>>>>> e6f2c6037f3a5291aedb4c7789442c751c85ce43
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
