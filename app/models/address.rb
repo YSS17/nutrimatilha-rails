2f36d58023ad48647f9d210a79ff220237b4b3d1
isabela porto <isamporto@hotmail.com>
2020-08-25 20:11:40 -0300

    Added validation to User, Pets, Subscription and Subs_product

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :user
+  validates :zip_code, :city, :state, :number, :neighborhood, :state, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  has_many :pets
-  has_one :address
+  has_many :pets, dependent: :destroy
+  has_one :address, dependent: :destroy
   has_one :subscription
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :adress, presence: true
 end
