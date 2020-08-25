01dd38653f8873020fb168441d15063a63301966
isabela porto <isamporto@hotmail.com>
2020-08-25 20:37:42 -0300

    Fixed schema and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,15 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2020_08_23_181104) do
 
   # These are extensions that must be enabled in order to support this database
@@ -26,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_08_23_181104) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "sex"
-    t.string "size"
+    t.integer "size"
     t.date "date_birth"
     t.integer "weight"
     t.boolean "neutered"
@@ -35,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_08_23_181104) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["breed_id"], name: "index_pets_on_breed_id"
+    t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -66,11 +79,6 @@ ActiveRecord::Schema.define(version: 2020_08_23_181104) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
-    t.integer "quantity"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
